4a6dd9e5353fb6dc813f9e091c5506bb89e677ff
pelldav <pelldav@gmail.com>
2013-02-07 13:27:53 +0100

    add package

---
+++ src/main/java/sisop/Test.java
@@ -0,0 +1,11 @@
+package sisop;
+
+import sisop.FairSemaphore.*;
+
+public class Test{
+    FairSemaphore sem = new FairSemaphore();
+
+    public static void main(String[] args) {
+        System.out.println("funziona");
+    }
+}

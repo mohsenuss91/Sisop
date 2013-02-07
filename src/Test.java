424662d6435f28e2769c0fc4432b56ffaad747d3
pelldav <pelldav@gmail.com>
2013-02-07 12:48:46 +0100

    Start First point

---
+++ src/Test.java
@@ -0,0 +1,11 @@
+package Sisop;
+
+import Sisop.FairSemaphore.*;
+
+public class Test{
+    FairSemaphore sem = new FairSemaphore();
+
+    public static void main(String[] args) {
+        System.out.println("funziona");
+    }
+}
